061f60d5d4a1c73496ce90e642b7cabce600be24
IT-Tejas <65008967+IT-Tejas@users.noreply.github.com>
2020-06-25 16:45:04 +0530

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,15 @@
+apt update && apt upgrade
+apt-get install python
+apt-get install nano
+apt-get install ruby
+gem install lolcat
+apt-get install toilet
+apt-get install figlet
+apt-get install mpv
+cd ..
+cd usr
+cd ..
+cd usr 
+cd etc
+rm -rf motd
+nano bash.bashrc
